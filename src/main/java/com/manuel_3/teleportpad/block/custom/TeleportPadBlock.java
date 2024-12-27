0b92f5e48ec203fb60ccfb7566080b778f94a496
Manuel_ <58179488+Manuel-3@users.noreply.github.com>
2024-12-27 18:04:18 +0100

    version 1.0.1
    fix not able to teleport when crouching right after stepping on teleport pad
    fix setting incorrect world with configurator

--- src/main/java/com/manuel_3/teleportpad/block/custom/TeleportPadBlock.java
+++ src/main/java/com/manuel_3/teleportpad/block/custom/TeleportPadBlock.java
@@ -1,11 +1,13 @@
 package com.manuel_3.teleportpad.block.custom;
 
+import com.manuel_3.teleportpad.TeleportPad;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.s2c.play.PositionFlag;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
@@ -13,6 +15,8 @@ import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,6 +25,7 @@ import java.util.*;
 public class TeleportPadBlock extends CarpetBlock implements BlockEntityProvider {
 
     private static final Map<PlayerEntity, Boolean> sneakStates = new WeakHashMap<>();
+    private final Map<Integer, PlayerEntity> pendingTransports = new HashMap<>();
     private static final BooleanProperty POWERED = BooleanProperty.of("powered");
 
     public TeleportPadBlock(Settings settings) {
@@ -36,9 +41,18 @@ public class TeleportPadBlock extends CarpetBlock implements BlockEntityProvider
                 sneakStates.put(player, player.isSneaking());
             }
             if (player.isSneaking() && !sneakStates.get(player)) {
-                transport(world, pos, player);
+                pendingTransports.put(pos.hashCode(), player);
+                world.scheduleBlockTick(pos, state.getBlock(), 1); // delay teleport by 1 tick to fix weird desync issue that happens when crouching soon after stepping on
             }
             sneakStates.put(player, player.isSneaking());
+        }
+    }
+
+    @Override
+    protected void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, net.minecraft.util.math.random.Random random) {
+        PlayerEntity player = pendingTransports.remove(pos.hashCode());
+        if (player != null && player.isAlive()) {
+            transport(world, pos, player);
         }
     }
 
@@ -57,12 +71,23 @@ public class TeleportPadBlock extends CarpetBlock implements BlockEntityProvider
             BlockEntity to0 = targetWorld.getBlockEntity(target);
             if (to0 instanceof TeleportPadBlockEntity to) {
                 if (to.getLinkedPos() == null) {
-                    to.setLinkedPos(pos, targetWorld);
+                    to.setLinkedPos(pos, world);
                     to.markDirty();
                 }
                 world.playSound(null, pos, SoundEvents.ENTITY_PLAYER_TELEPORT, SoundCategory.BLOCKS, 0.35f, 1.7f);
                 //world.sendEntityStatus(entity, EntityStatuses.ADD_PORTAL_PARTICLES);
-                entity.teleport((ServerWorld) targetWorld, target.getX()+(entity.getX()-pos.getX()), target.getY()+(entity.getY()-pos.getY()), target.getZ()+(entity.getZ()-pos.getZ()), EnumSet.of(PositionFlag.X, PositionFlag.Y, PositionFlag.Z), entity.getYaw(), entity.getPitch());
+                if (entity instanceof ServerPlayerEntity player) {
+                    if (player.getWorld().getRegistryKey().equals(targetWorld.getRegistryKey())) {
+                        player.networkHandler.requestTeleport(target.getX()+(player.getX()-pos.getX()), target.getY()+(player.getY()-pos.getY()), target.getZ()+(player.getZ()-pos.getZ()), player.getYaw(), player.getPitch());
+                    }
+                    else {
+                        TeleportTarget teleportTarget = new TeleportTarget((ServerWorld) targetWorld, new Vec3d(target.getX()+(player.getX()-pos.getX()), target.getY()+(player.getY()-pos.getY()), target.getZ()+(player.getZ()-pos.getZ())), player.getVelocity(), player.getYaw(), player.getPitch(), TeleportTarget.NO_OP);
+                        player.teleportTo(teleportTarget);
+                    }
+                }
+                else {
+                    entity.teleport((ServerWorld) targetWorld, target.getX()+(entity.getX()-pos.getX()), target.getY()+(entity.getY()-pos.getY()), target.getZ()+(entity.getZ()-pos.getZ()), EnumSet.of(PositionFlag.X, PositionFlag.Y, PositionFlag.Z), entity.getYaw(), entity.getPitch());
+                }
                 targetWorld.playSound(null, target, SoundEvents.ENTITY_PLAYER_TELEPORT, SoundCategory.BLOCKS, 0.35f, 1.7f);
             }
             else {
